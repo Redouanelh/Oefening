f5d232cf9cea618ba30e0c826e34e01f663236d4
Redouanelh <redouan_school@outlook.com>
2018-09-18 09:57:49 +0200

    Oefeningen

--- pe5_1.py
+++ pe5_1.py
@@ -1,6 +1,6 @@
 score = eval(input('Geef de behaalde score: '))
 
-if score > 15:
+if score >= 15:
     print('Gefeliciteerd, je bent geslaagd!')
     print('Je bent namelijk geslaagd bij een score hoger dan 15!')
 
